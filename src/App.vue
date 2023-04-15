3c047c05656912200e2c2b9b07f37682bc5af714
iTzMvz <matheus.vidal.zenere@gmail.com>
2023-04-14 23:49:03 -0300

    Finalizado

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,15 @@ const genero = ref('')
 const bio = ref('')
 const hobbies = ref('')
 const linguagemProgramacao = ref([])
+const mensagem = ref('')
 const enviar = ref(false)
 
 
 function confirmar() {
   if (senha.value != confirmaSenha.value) {
-    alert('senha incorreta')  
+    mensagem.value = 'A senha está incorreta'
   } else {
-    return enviar.value = !enviar.value
+  return enviar.value = !enviar.value
   }
 }
 </script>
@@ -32,31 +33,31 @@ function confirmar() {
       <h1>Insira Suas Informações</h1>
       <div class="input-field">
         <label>Nome</label>
-        <input type="text" v-model="nome"  placeholder="Nome" minlength="1" maxlength="40 " />
+        <input type="text" v-model="nome"  placeholder="Nome" minlength="1" maxlength="40 " required  />
       </div>
       <div class="input-field">
         <label>Email</label>
-        <input type="email" v-model="email" minlength="1" placeholder="Email" />
+        <input type="email" v-model="email" minlength="1" placeholder="Email" required  />
       </div>
       <div class="input-field">
         <label>Senha:</label>
-        <input type="password" v-model="senha" minlength="1" maxlength="20" placeholder="Senha" />
+        <input type="password" v-model="senha" minlength="1" maxlength="20" placeholder="Senha" required />
       </div>
       <div class="input-field">
         <label>Confirme a Senha:</label>
-        <input type="password" v-model="confirmaSenha" minlength="1" maxlength="20" placeholder="Confirmar senha" />
+        <input type="password" v-model="confirmaSenha" minlength="1" maxlength="20" placeholder="Confirmar senha" required  />
       </div>
       <div class="input-field">
         <label>Endereço:</label>
-        <input type="text" v-model="endereco" minlength="1" placeholder="Endereço" />
+        <input type="text" v-model="endereco" minlength="1" placeholder="Endereço" required  />
       </div>
       <div class="input-field">
         <label>Cidade:</label>
-        <input type="text" v-model="cidade" minlength="1" placeholder="Cidade" />
+        <input type="text" v-model="cidade" minlength="1" placeholder="Cidade" required  />
       </div>
       <div class="input-field">
         <label>Estado</label>
-        <select style="margin-top: 0" v-model="estado">
+        <select style="margin-top: 0" v-model="estado" required >
           <option value="">Estado</option>
           <option value="AC">Acre</option>
           <option value="AL">Alagoas</option>
@@ -89,7 +90,7 @@ function confirmar() {
       </div>
       <div class="input-field">
         <label>Data de nascimento:</label>
-        <input type="date" v-model="data" min="1920-01-01" max="2023-04-14" />
+        <input type="date" v-model="data" min="1920-01-01" max="2023-04-14" required  />
       </div>
       <div class="input-field">
         <label>Gênero:</label>
@@ -128,14 +129,16 @@ function confirmar() {
       </div>
       <div class="input-field">
         <label>Hobbies:</label>
-        <input type="text" v-model="hobbies" minlength="1" placeholder="Hobbies">
+        <input type="text" v-model="hobbies" minlength="1" placeholder="Hobbies" required >
       </div>
       <div class="input-field">
         <label>Biografia:</label>
-        <textarea v-model="bio" minlength="1" maxlength="200" placeholder="Biografia"></textarea>
+        <textarea v-model="bio" minlength="1" maxlength="200" placeholder="Biografia" required ></textarea>
       </div>
       <button type="submit" value="confirm">Enviar</button>
+      <p style="color: red;">{{ mensagem }}</p>
     </div>
+  </form>
     <div class="info">
       <div v-if="enviar">
         <h1>Suas Informações</h1>
@@ -149,10 +152,9 @@ function confirmar() {
         <p class="box">Gênero: {{ genero }}</p>
         <p class="box">Hobbie: {{ hobbies }}</p>
         <p class="box">Linguagem de Programação: {{ linguagemProgramacao.join(", ") }}</p>
-        <p class="box">Biografia: {{ bio }}</p>
+        <p class="box" style="width: 332px;">Biografia: {{ bio }}</p>
       </div>
     </div>
-  </form>
   </div>
 </template>
 
